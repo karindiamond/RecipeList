8e21bb9ad860865f69c0e0aa8e12628dbe7a7a97
efrederic <ethan.frederic@gmail.com>
2018-05-01 23:52:00 -0700

    - populating link field when editing - removed ExpandableListView - more cleanup

--- app/src/main/java/com/kld2/recipelist/RecipeActivity.java
+++ app/src/main/java/com/kld2/recipelist/RecipeActivity.java
@@ -64,6 +64,7 @@ public class RecipeActivity extends AppCompatActivity {
                             }
                         })
                         .show();
+                linkText.setText(recipeLink.getText());
                 return true;
             }
         });
