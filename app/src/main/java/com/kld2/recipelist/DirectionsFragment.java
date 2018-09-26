1d367c72d2313b2fc7b34829a6b7aa97df44a464
efrederic <ethan.frederic@gmail.com>
2018-09-25 20:02:13 -0700

    - gradle updates - API level updated to Android Pie - migration to androidx libs

--- app/src/main/java/com/kld2/recipelist/DirectionsFragment.java
+++ app/src/main/java/com/kld2/recipelist/DirectionsFragment.java
@@ -2,13 +2,13 @@ package com.kld2.recipelist;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,7 +18,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class DirectionsFragment extends Fragment {
--- app/src/main/java/com/kld2/recipelist/NewRecipeActivity.java
+++ app/src/main/java/com/kld2/recipelist/NewRecipeActivity.java
@@ -1,7 +1,7 @@
 package com.kld2.recipelist;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
